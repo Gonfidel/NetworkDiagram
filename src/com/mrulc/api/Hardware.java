ebf99d25931c8a0d4cba145dc5f151fbc905f949
Tyson Duncan <Tyson.121@hotmail.com>
2018-09-29 15:02:09 -0600

    Added HardWareList class

--- src/com/mrulc/api/Hardware.java
+++ src/com/mrulc/api/Hardware.java
@@ -1,13 +1,58 @@
 package com.mrulc.api;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-
+import java.lang.reflect.Constructor;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class Hardware {
+
     private String name;
-    private String ip;
+    private InetAddress ip;
+    private InetAddress subNet;
+
+
+/* *****************************************************************
+                           CONSTRUCTOR
+ ***************************************************************** */
+    public Hardware(String name, String ip, String subNet) throws UnknownHostException{
+         this.name = name;
+         this.ip = InetAddress.getByName(ip);
+         this.subNet = InetAddress.getByName(subNet);
+}
+/* *************************************************************** */
+
+    public void setName(String name) {
+        this.name = name;
+}
+
+    public void setIp(String ip) throws UnknownHostException {
+        this.ip = InetAddress.getByName(ip);
+    }
+
+    public void setSubNet(String subNet) throws UnknownHostException{
+        this.subNet = InetAddress.getByName(subNet);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public InetAddress getIp() {
+
+        return ip;
+    }
+
+    public InetAddress getSubNet() {
+        return subNet;
+    }
+
+
+
+
+
+
+
+
+
 
 }
---
+++ src/com/mrulc/api/view/HardwareList.java
@@ -0,0 +1,4 @@
+package com.mrulc.api.view;
+
+public class HardwareList {
+}
